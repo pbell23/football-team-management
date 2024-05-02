138fc4638e03627839fde13358a32d11a001aae5
pkzvh <ppaul.bellamy@gmail.com>
2024-05-02 02:37:02 +0200

    feat: add di development config

--- src/shared/di/config-loader.ts
+++ src/shared/di/config-loader.ts
@@ -2,6 +2,8 @@ import { IConfigTypes } from './config-types.js';
 
 async function loadConfig(env: string): Promise<IConfigTypes> {
     switch (env) {
+        case 'development':
+            return import('./development-config.js').then(config => config.default);
         case 'testing':
             return import('./testing-config.js').then(config => config.default);
         default:

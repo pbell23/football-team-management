bfc86705c71fc903bc59f613b1c3c52eb1ed71ff
pkzvh <ppaul.bellamy@gmail.com>
2024-05-02 18:06:07 +0200

    feat: Add user-related modules to development and testing config

--- src/shared/di/testing-config.ts
+++ src/shared/di/testing-config.ts
@@ -1,12 +1,19 @@
+import { AuthController } from '../../modules/auth/auth.controller.js';
 import { IConfigTypes } from './config-types.js';
 import { InMemoryPlayerRepository } from '../../modules/player/in-memory-player.repository.js';
+import { InMemoryUserRepository } from '../../modules/auth/in-memory-user.repository.js';
+import { PasswordService } from '../../modules/auth/password.service.js';
 import { PlayerController } from '../../modules/player/player.controller.js';
 import { PlayerService } from '../../modules/player/player.service.js';
+import { UserService } from '../../modules/auth/user.service.js';
 
 const testingConfig: IConfigTypes = {
     playerRepository: new InMemoryPlayerRepository(),
     playerService: new PlayerService(new InMemoryPlayerRepository()),
     playerController: new PlayerController(new PlayerService(new InMemoryPlayerRepository())),
+    userRepository: new InMemoryUserRepository(),
+    userService: new UserService(new InMemoryUserRepository(), new PasswordService()),
+    authController: new AuthController(new UserService(new InMemoryUserRepository(), new PasswordService())),
 };
 
 export default testingConfig;

62a406a631b637ba6e083d736c267ac11d65546a
pkzvh <ppaul.bellamy@gmail.com>
2024-05-02 18:05:09 +0200

    feat: Add user-related interfaces and import statements

--- src/shared/di/config-types.ts
+++ src/shared/di/config-types.ts
@@ -1,9 +1,15 @@
+import { AuthController } from '../../modules/auth/auth.controller.js';
 import { IPlayerRepository } from '../../modules/player/player.interface.js';
 import { IPlayerService } from '../../modules/player/player.interface.js';
+import { IUserRepository } from '../../modules/auth/interface/user-repository.interface.js';
+import { IUserService } from '../../modules/auth/interface/user-service.interface.js';
 import { PlayerController } from '../../modules/player/player.controller.js';
 
 export interface IConfigTypes {
     playerRepository: IPlayerRepository;
     playerService: IPlayerService;
     playerController: PlayerController;
+    userRepository: IUserRepository;
+    userService: IUserService;
+    authController: AuthController;
 }
